b5b7e4b3414a3a7c5f9f0410949bbbbbe31c7a8e
Sergey992 <81615755+Sergey992@users.noreply.github.com>
2021-08-09 21:03:05 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,27 @@
-<html>
-{% load static %}
-<body background = {% static "mywebpage2/1.jpg" %} >
-<center>
-<font color="yelow" >
-<br>
-<br>
-<br>
-<p>
-<h1><p><p><p>
-		Burger oh-may Ver-2!!
-</h1>
-<font color="red">
-<img src={% static "mywebpage2/2.jpg" height=500 weight=350 border=2 %} >
+<HTML>
+<HEAD>
+<TITLE>Web-Site-Title</TITLE>
+<SCRIPT LANGUAGE="JavaScript">
+     var sizes = new Array(0,1,2,4,8,10,12);
+     sizes.pos = 0;
+
+function Elastic()
+{
+     var el = document.all.elastic
+     if (null == el.direction)el.direction =1
+     else if ((sizes.pos > sizes.length -2) || (0 == sizes.pos))
+     el.direction *= -1
+     el.style.letterSpacing = sizes[sizes.pos += el.direction]
+setTimeout('Elastic()',100)
+}
+
+</SCRIPT>
+<BODY bgcolor="gray" onLoad=Elastic()>
+<CENTER>
+<br><br><br><br>
+<br><br><br><br>
+<font color="green"><h2>Hello From</h2>
+<font color="gold"><H1 ID="elastic" ALIGN="Center">GitHub ACTIONS!!!</H1>
 
 </body>
-</html>
+</HTML>
