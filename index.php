473f2fee155ace413ef9e557cac8fe87e53a69cd
Sergey992 <81615755+Sergey992@users.noreply.github.com>
2021-08-09 20:59:32 +0300

    index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+{% load static %}
+<body background = {% static "mywebpage2/1.jpg" %} >
+<center>
+<font color="yelow" >
+<br>
+<br>
+<br>
+<p>
+<h1><p><p><p>
+		Burger oh-may Ver-2!!
+</h1>
+<font color="red">
+<img src={% static "mywebpage2/2.jpg" height=500 weight=350 border=2 %} >
+
+</body>
+</html>
